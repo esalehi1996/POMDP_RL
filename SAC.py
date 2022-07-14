fixed GMM softmax and test epsilon

SAC.py
@@ -175,6 +175,9 @@ class SAC(object):
                 sample = random.random()
                 if sample < eps_threshold and evaluate is False:
                     return torch.tensor([[random.randrange(self.act_dim)]],dtype=torch.long).cpu().numpy()[0][0] , hidden_p
+                if sample < self.args['test_epsilon'] and evaluate is True:
+                    return torch.tensor([[random.randrange(self.act_dim)]], dtype=torch.long).cpu().numpy()[0][
+                               0], hidden_p
             if self.rl_alg == 'SAC':
                 action, pi, _ = self.policy_cpu.sample(ais_z.detach())
                 # print(action)
@@ -767,7 +770,7 @@ class SAC(object):
 
                     mixture_probs = torch.sum(m.log_prob(target), 2) + torch.log(mvg_dist_mix)
 
-                    # print(mixture_probs.shape)
+                    # print(mvg_dist_mix.shape,mvg_dist_mix)
                     #
                     # print(mixture_probs)
                     #

models.py
@@ -76,7 +76,7 @@ class psi_net_highdim_KL(nn.Module):
         input_ndims = AIS_state_size + num_actions
         self.eps = 1e-6
         self.num_components = num_components
-        self.softmax = nn.Softmax(dim=0)
+        self.softmax = nn.Softmax(dim=1)
 
         self.fc1_r = nn.Linear(input_ndims, AIS_state_size//2)
         self.fc2_r = nn.Linear(AIS_state_size//2, 1)