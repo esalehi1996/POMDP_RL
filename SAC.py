6d014b483040b12d0fe141937dd743eae0375aaa
esalehi1996 <45656101+esalehi1996@users.noreply.github.com>
2022-10-03 16:34:27 -0400

    Update SAC.py

--- SAC.py
+++ SAC.py
@@ -343,7 +343,7 @@ class SAC(object):
 
 
 
-                qf_loss = F.SmoothL1Loss(qf.view(-1), next_q_value , reduce = False)
+                qf_loss = F.smooth_l1_loss(qf.view(-1), next_q_value , reduce = False)
 
                 # print(qf_loss)
 
@@ -777,7 +777,7 @@ class SAC(object):
 
             # print(qf.shape)
             # print(qf)
-            qf_loss = F.SmoothL1Loss(qf.view(-1), next_q_value, reduce=False)
+            qf_loss = F.smooth_l1_loss(qf.view(-1), next_q_value, reduce=False)
             if self.args['PER'] is True:
                 is_weight_td = torch.from_numpy(is_weight_td).to(self.device)
                 is_weight_td_packed = pack_padded_sequence(is_weight_td,
